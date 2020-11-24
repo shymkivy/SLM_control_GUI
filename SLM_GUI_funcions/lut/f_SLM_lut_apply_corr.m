d89f15c3f863c380ef491db8413dd771610f0a5f
yuriy <shymkivy@gmail.com>
2020-11-23 22:07:11 -0500

    lut corrections

--- SLM_GUI_funcions/lut/f_SLM_lut_apply_corr.m
+++ SLM_GUI_funcions/lut/f_SLM_lut_apply_corr.m
@@ -3,7 +3,8 @@ function pointer_out = f_SLM_lut_apply_corr(app, pointer, region)
 idx_reg = strcmpi(region, [app.region_list.name_tag]);
 if ~isempty(app.region_list(idx_reg).lut_correction)
     idx_lut_corr = strcmpi(app.region_list(idx_reg).lut_correction(:,1), app.SLM_ops.lut_fname);
-    lut_correction_data = app.region_list(idx_reg).lut_correction{idx_lut_corr,2};
+    lut_corr_idx = strcmpi(app.lut_corrections_list(:,1), app.region_list(idx_reg).lut_correction{idx_lut_corr,2});
+    lut_correction_data = app.lut_corrections_list{lut_corr_idx,2};
 else
     lut_correction_data = [];
 end
