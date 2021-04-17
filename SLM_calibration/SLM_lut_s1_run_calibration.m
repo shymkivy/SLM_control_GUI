860627d5463de53a6d75be044ba05a22f73b6445
yuriy <shymkivy@gmail.com>
2021-04-17 18:00:40 -0400

    bns 512

--- SLM_calibration/SLM_lut_s1_run_calibration.m
+++ SLM_calibration/SLM_lut_s1_run_calibration.m
@@ -37,7 +37,7 @@ ops.SLM_type = 1; % 1 = BNS 512 with OverDrive (OD)
 
 if ops.SLM_type == 1
     % Prairie 1
-    ops.SLM_SDK_dir = 'C:\Program Files\Meadowlark Optics\Blink\OverDrive Plus\SDK';
+    ops.SLM_SDK_dir = 'C:\Program Files\Meadowlark Optics\Blink\SDK';
     % 901D
     %ops.SLM_SDK_dir = 'C:\Program Files\Meadowlark Optics\Blink OverDrive Plus\SDK';
     %ops.init_reg_lut_fname = 'SLM_3329_20150303.txt';
