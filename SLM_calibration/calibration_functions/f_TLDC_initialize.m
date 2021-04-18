0054dcedba531c8756e37d05b5e2c521b12ac70b
yuriy <shymkivy@gmail.com>
2021-04-18 19:33:49 -0400

    bns 512

--- SLM_calibration/calibration_functions/f_TLDC_initialize.m
+++ SLM_calibration/calibration_functions/f_TLDC_initialize.m
@@ -1,7 +1,7 @@
 function [cam_out, cam_params] = f_TLDC_initialize(ops)
 % Set up the camera
 
-cam_params.path_TLCAM_MEX = [ops.working_dir '\..\..\TLDC_MEX'];
+cam_params.path_TLCAM_MEX = [ops.working_dir '\..\..\libraries\TLDC_MEX'];
 addpath(cam_params.path_TLCAM_MEX);
 
 % Camera parameters
