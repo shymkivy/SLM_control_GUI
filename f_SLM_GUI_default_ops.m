90bc4af3e8f1860a45c65274fd88f2712abddc15
yuriy <shymkivy@gmail.com>
2021-04-18 17:44:34 -0400

    bns 512

--- f_SLM_GUI_default_ops.m
+++ f_SLM_GUI_default_ops.m
@@ -26,15 +26,15 @@ ops.AO_recording_dir = ''; % E:\data\SLM\AO\12_4_20\zernike_100um_1modes-001
 
 %% default lut
 if ops.SLM_type == 0
-    % Prairie 1, sdk with no overdrive
-    %ops.SLM_SDK_dir = 'C:\Program Files\Meadowlark Optics\Blink\SDK';
-    % 901D, with overdrive
-    ops.SLM_SDK_dir = 'C:\Program Files\Meadowlark Optics\Blink OverDrive Plus\SDK';
-    
     %ops.lut_fname =  'linear.lut'; %'photodiode_lut_comb_1064L_940R_64r_11_12_20_from_linear.txt';
     %ops.lut_fname =  'photodiode_lut_comb_1064L_940R_64r_11_12_20_from_linear.txt';
     ops.lut_fname =  'photodiode_lut_940_1r_11_10_20_14h_37m_from_linear.lut';
 else
+    % Prairie 1, sdk with no overdrive
+    ops.SLM_SDK_dir = 'C:\Program Files\Meadowlark Optics\Blink\SDK';
+    % 901D, with overdrive
+    %ops.SLM_SDK_dir = 'C:\Program Files\Meadowlark Optics\Blink OverDrive Plus\SDK';
+    
     ops.lut_fname = 'linear.lut';
     ops.init_lut_fname =  'SLM_3329_20150303.txt'; % SLM_3329_20150303.txt; slm4317_test_regional.txt
 end
