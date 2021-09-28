29e5e300355313b7c9947cb6f7a35ec4bc9ed304
yuriy <shymkivy@gmail.com>
2021-09-28 15:53:33 -0400

    load ops update

--- SLM_GUI_funcions/f_sg_ops_load.m
+++ SLM_GUI_funcions/f_sg_ops_load.m
@@ -5,9 +5,22 @@ fname = 'SLM_GUI_local_ops.mat';
 if exist(fname, 'file')
     load_data = load(fname, 'saved_data');
     load_data = load_data.saved_data;
+    
+    temp_xyz = load_data.xyz_patterns;
+    for n_pat = 1:numel(temp_xyz)
+        if size(temp_xyz(n_pat).xyz_pts,2)>6
+            temp_pts = temp_xyz(n_pat).xyz_pts;
+            temp_pts(:,6) = [];
+            temp_pts(:,3) = temp_xyz(n_pat).xyz_pts(:,4);
+            temp_pts(:,4) = temp_xyz(n_pat).xyz_pts(:,5);
+            temp_pts(:,5) = temp_xyz(n_pat).xyz_pts(:,3);
+            temp_xyz(n_pat).xyz_pts = temp_pts;
+        end
+    end
 
+    
     app.region_list = load_data.region_list;
-    app.xyz_patterns = load_data.xyz_patterns;
+    app.xyz_patterns = temp_xyz;
     
     f_sg_load_calibration(app)
     
