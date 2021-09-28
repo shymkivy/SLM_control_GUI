7d43b19d00e6b97f253973fb1023d5726aff7318
yuriy <shymkivy@gmail.com>
2021-09-28 15:02:02 -0400

    bug

--- SLM_GUI_funcions/xyz_patterns/f_sg_xyz_button_view_selected_fft.m
+++ SLM_GUI_funcions/xyz_patterns/f_sg_xyz_button_view_selected_fft.m
@@ -6,7 +6,7 @@ if size(app.UIImagePhaseTableSelection,1) > 0
     % get region
     [m_idx, n_idx] = f_sg_get_reg_deets(app, app.CurrentregionDropDown.Value);
 
-    holo_image = f_sg_xyz_gen_holof_sg_xyz_gen_holo(app, coord, app.CurrentregionDropDown.Value); 
+    holo_image = f_sg_xyz_gen_holo(app, coord, app.CurrentregionDropDown.Value); 
 
     f_sg_view_hologram_fft(app, holo_image(m_idx, n_idx), app.fftdefocusumEditField.Value*1e-6);
     title(sprintf('PSF at %.1f um', app.fftdefocusumEditField.Value));
