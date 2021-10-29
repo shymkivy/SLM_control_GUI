fa2aa7e78d3f13320b05e0fe81b181479252f816
yuriy <shymkivy@gmail.com>
2021-10-29 16:44:35 -0400

    XYZ updates

--- SLM_GUI_funcions/f_sg_load_default_ops.m
+++ SLM_GUI_funcions/f_sg_load_default_ops.m
@@ -1,11 +1,11 @@
 function f_sg_load_default_ops(app)
 
 %% copy specific SLM params
-fields1 = app.SLM_ops.SLM_params(strcmpi({app.SLM_ops.SLM_params.name}, app.SLM_ops.SLM_type));
-fieldnames1 = fieldnames(fields1);
-for n_fl = 1:numel(fieldnames1)
-    if ~isempty(fields1.(fieldnames1{n_fl}))
-        app.SLM_ops.((fieldnames1{n_fl})) = fields1.(fieldnames1{n_fl});
+SLM_params = app.SLM_ops.SLM_params(strcmpi({app.SLM_ops.SLM_params.name}, app.SLM_ops.SLM_type));
+SLM_fieldnames = fieldnames(SLM_params);
+for n_fl = 1:numel(SLM_fieldnames)
+    if ~isempty(SLM_params.(SLM_fieldnames{n_fl}))
+        app.SLM_ops.((SLM_fieldnames{n_fl})) = SLM_params.(SLM_fieldnames{n_fl});
     end
 end
 app.SLMtypeDropDown.Items = {app.SLM_ops.SLM_params.name};
@@ -15,13 +15,24 @@ app.SLMtypeDropDown.Value = app.SLM_ops.SLM_type;
 %% Objective params
 app.ObjectiveDropDown.Items = unique({app.SLM_ops.obj_params.name},'stable');
 
-app.SLM_ops.obj_params
+obj_params = app.SLM_ops.obj_params(strcmpi({app.SLM_ops.obj_params.SLM},app.SLMtypeDropDown.Value));
+obj_params = app.SLM_ops.obj_params(strcmpi({obj_params.name},app.ObjectiveDropDown.Value));
 
-%% copy regions
-app.region_list
+%% copy regions data
 
-[app.SLM_ops.region_list.name_tag]
+region_list = [];
+for n_reg = 1:numel(SLM_params.regions_use)
+    reg1 = app.SLM_ops.region_list(strcmpi(SLM_params.regions_use(n_reg), [app.SLM_ops.region_list.name_tag]));
+    temp_params = obj_params(strcmpi(reg1.name_tag, {obj_params.region}));
+    if ~isempty(temp_params)
+        reg2 = f_copy_fields(reg1, temp_params);
+    else
+    end
+    region_list = [region_list; reg2];
+end
 
+
+app.region_list = region_list;
 
 %% copy patterns
 
