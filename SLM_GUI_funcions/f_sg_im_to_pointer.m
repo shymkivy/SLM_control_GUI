94f1d5fc681064dfb9d784d1959179b91f3fdd52
yuriy <shymkivy@gmail.com>
2021-11-07 18:30:48 -0500

    major overhaul

--- SLM_GUI_funcions/f_sg_im_to_pointer.m
+++ SLM_GUI_funcions/f_sg_im_to_pointer.m
@@ -1,6 +1,6 @@
 function holo_pointer_value = f_sg_im_to_pointer(holo_image)
 
-temp_holo = uint8((rot90(holo_image, 3)/(2*pi))*255);
-holo_pointer_value = reshape(temp_holo, [],1);
+temp_holo = uint8((holo_image/(2*pi))*255);
+holo_pointer_value = reshape(temp_holo', [],1);
 
 end
