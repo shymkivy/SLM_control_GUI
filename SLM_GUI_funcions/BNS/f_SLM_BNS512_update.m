be9b182deda95740db84c2ff41b2ae69c4d3c3f3
yuriy <shymkivy@gmail.com>
2022-05-01 03:01:19 -0400

    updates

--- SLM_GUI_funcions/BNS/f_SLM_BNS512_update.m
+++ SLM_GUI_funcions/BNS/f_SLM_BNS512_update.m
@@ -1,10 +1,12 @@
 function f_SLM_BNS512_update(ops, image_pointer)
 
+im_out = f_sg_poiner_to_im(image_pointer, ops.height, ops.width);
 % loads image
-x = calllib('Blink_SDK_C', 'Write_image', ops.sdk, ops.board_number, image_pointer, ops.width*ops.height, ops.wait_For_Trigger, ops.external_Pulse);
+out1 = calllib('Blink_SDK_C', 'Write_image', ops.sdk, ops.board_number, im_out, ops.width*ops.height, ops.wait_For_Trigger, ops.external_Pulse);
 % checks if image is complete
 % calllib('Blink_SDK_C', 'Is_slm_transient_constructed', ops.sdk);
-if ~x
+
+if ~out1
     disp(calllib('Blink_SDK_C', 'Get_last_error_message', ops.sdk));
 end
 
