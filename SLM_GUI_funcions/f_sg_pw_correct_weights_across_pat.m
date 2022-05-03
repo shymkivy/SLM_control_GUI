66542e7525b795774da4a15a2fcca5442344fe0c
yuriy <shymkivy@gmail.com>
2022-05-02 22:54:15 -0400

    updates

--- SLM_GUI_funcions/f_sg_pw_correct_weights_across_pat.m
+++ SLM_GUI_funcions/f_sg_pw_correct_weights_across_pat.m
@@ -2,63 +2,83 @@ function f_sg_pw_correct_weights_across_pat(app)
 
 % zero zero zero is a special location used for zero order block
 
-f_sg_pw_correct_weights_within_pat(app)
+tab_data_full = app.UIImagePhaseTable.Data;
 
-tab_data = app.UIImagePhaseTable.Data;
+f_sg_pw_correct_weights_within_pat(app, unique(tab_data_full.Pattern))
+
+tab_data_full = app.UIImagePhaseTable.Data;
+
+current_idx = max(tab_data_full.Idx)+1;
 
 [~, ~, reg1] = f_sg_get_reg_deets(app, app.CurrentregionDropDown.Value);
+% coord_zero.xyzp = [0 0 0];
+% coord_zero.weight = 0;
+% coord_zero.NA = reg1.effective_NA;
+% data_w_zero = f_sg_simulate_weights(reg1, zeros(reg1.SLMm, reg1.SLMn), coord_zero);
+
+coord_bd.xyzp = [reg1.beam_dump_xy 0];
+coord_bd.weight = 0;
+coord_bd.NA = reg1.effective_NA;
+[holo_phase_bd, coord_bd_corr] = f_sg_xyz_gen_holo(coord_bd, reg1);
 
 % find zeros and remove
-beam_dump_idx = and(and(tab_data.X == reg1.beam_dump_xy(1), tab_data.Y == reg1.beam_dump_xy(2)), tab_data.Z == 0);
-% tab_data(zero_idx,:) = [];
-% app.UIImagePhaseTable.Data = tab_data;
-
-% recompute all power distributions
-%[tab_data, powers_all] = f_sg_update_table_power(app);
+beam_dump_idx = and(and(tab_data_full.X == reg1.beam_dump_xy(1), tab_data_full.Y == reg1.beam_dump_xy(2)), tab_data_full.Z == 0);
+tab_data = tab_data_full(~beam_dump_idx,:);
+tab_data_bd = tab_data_full(beam_dump_idx,:);
 
 all_pat = unique(tab_data.Pattern);
 
+[~, min_pow_idx] = min(tab_data.Power);
+tab_data_target = tab_data;
+tab_data_target.Power = ones(numel(tab_data_target.Power),1)*tab_data.Power(min_pow_idx);
+
 power_corr = f_sg_apply_xy_power_corr(reg1.pw_corr_data, [tab_data.X, tab_data.Y]);
-tab_data_pre_corr = tab_data;
-tab_data_pre_corr.Power = tab_data.Power./power_corr;
+tab_data_target_pre_corr = tab_data_target;
+tab_data_target_pre_corr.Power = tab_data_target.Power./power_corr;
 
-[min_power, min_pow_idx] = min(tab_data_pre_corr.Power(~beam_dump_idx));
-
-tab_data_pre_corr_target = tab_data;
-tab_data_pre_corr_target.Power = tab_data.Power(min_pow_idx)./power_corr;
-
-new_row = f_sg_initialize_tabxyz(app, 1);
-tab_new = f_sg_initialize_tabxyz(app, 0);
+new_row_bd = f_sg_initialize_tabxyz(app, 1);
+new_row_bd.X = reg1.beam_dump_xy(1);
+new_row_bd.Y = reg1.beam_dump_xy(2);
 
 for n_pat = 1:numel(all_pat)
     curr_pat = all_pat(n_pat);
-    tab_pat = tab_data_pre_corr_target(tab_data_pre_corr_target.Pattern == curr_pat,:);
+    tab_pat = tab_data_target_pre_corr(tab_data_target_pre_corr.Pattern == curr_pat,:);
     
-    weight1 = tab_pat.Weight;
-    weight1 = weight1/sum(weight1);
+    coord.xyzp = [tab_pat.X tab_pat.Y tab_pat.Z];
+    coord.weight = tab_pat.Weight;
+    coord.NA = reg1.effective_NA;
+
+    [holo_phase, coord_corr] = f_sg_xyz_gen_holo(coord, reg1);
     
-    power_pre_corr = tab_pat.Power./power_corr;
+    I_target = tab_pat.Power;
+    w_out = f_sg_optimize_phase_w_bd(app, holo_phase, holo_phase_bd, coord_corr, coord_bd_corr, I_target);
     
-    weight2 = min_power./tab_pat.Power .* weight1;
-    weight_zero = 1 - sum(weight2);
+    tab_pat.Weight = w_out.w_final;
+    tab_pat.Power = w_out.I_final;
     
-    weight_all = [weight2;weight_zero];
-    
-    weight_all = weight_all/min(weight2);
-    
-    new_row.Pattern = curr_pat;
-    tab_data2 = [tab_data(tab_data.Pattern == curr_pat,:); new_row];
-    tab_data2.Weight = weight_all;
-    
-    [tab_data3, ~] = f_sg_update_table_power_core(reg1, tab_data2);
-    
-    tab_new = [tab_new; tab_data3];
+    pat_bd_idx = tab_data_bd.Pattern == curr_pat;
+    if ~sum(pat_bd_idx)
+        new_row_bd.Pattern = curr_pat;
+        new_row_bd.Idx = current_idx;
+        current_idx = current_idx + 1;
+        tab_data_bd = [tab_data_bd; new_row_bd];
+    end
+    pat_bd_idx = tab_data_bd.Pattern == curr_pat;
+    tab_data_bd(pat_bd_idx,:).Weight = w_out.wbd_final;
+    tab_data_bd(pat_bd_idx,:).Power = w_out.Ibd_final;
+
+    tab_data(tab_data.Pattern == curr_pat,:) = tab_pat;
 end
 
-tab_new.Idx = (1:numel(tab_new.Idx))';
+tab_data_full2 = [tab_data; tab_data_bd];
 
-app.UIImagePhaseTable.Data = tab_new;
+tab_data_full3 = sortrows(tab_data_full2,'Idx');
 
+power_corr = f_sg_apply_xy_power_corr(reg1.pw_corr_data, [tab_data_full3.X, tab_data_full3.Y]);
 
+tab_data_full4 = tab_data_full3;
+tab_data_full4.Power = tab_data_full3.Power .* power_corr;
+
+app.UIImagePhaseTable.Data = tab_data_full4;
 
 end
--- SLM_GUI_funcions/f_sg_update_table_power.m
+++ SLM_GUI_funcions/f_sg_update_table_power.m
@@ -9,33 +9,34 @@ end
 [~, ~, reg1] = f_sg_get_reg_deets(app, app.CurrentregionDropDown.Value);
 reg1 = f_sg_get_reg_extra_deets(reg1);
 
+coord_zero.xyzp = [0 0 0];
+coord_zero.weight = 0;
+coord_zero.NA = reg1.effective_NA;
+data_w_zero = f_sg_simulate_weights(reg1, zeros(reg1.SLMm, reg1.SLMn), coord_zero);
+
 powers_all = cell(numel(curr_pat_all),1);
 
 for n_pat = 1:numel(curr_pat_all)
     curr_pat = curr_pat_all(n_pat);
     
     tab_data_pat = tab_data(tab_data.Pattern == curr_pat,:);
+    tab_data_pat.Weight = tab_data_pat.Weight./sum(tab_data_pat.Weight);
     
     coord.xyzp = [tab_data_pat.X, tab_data_pat.Y, tab_data_pat.Z];
     coord.weight = tab_data_pat.Weight;
     coord.NA = reg1.effective_NA;
     
-    coord_zero = coord;
-    coord_zero.xyzp = [0 0 0];
-    
     [holo_phase, coord_corr] = f_sg_xyz_gen_holo(coord, reg1);
     
     SLM_phase = angle(sum(exp(1i*(holo_phase)).*reshape(coord.weight,[1 1 numel(coord_corr.weight)]),3));
     
-    data_w_zero = f_sg_simulate_weights(reg1, zeros(size(SLM_phase)), coord_zero);
     data_w = f_sg_simulate_weights(reg1, SLM_phase, coord_corr);
     
     power_sim = data_w.pt_mags/data_w_zero.pt_mags;
-    
     power_corr = f_sg_apply_xy_power_corr(reg1.pw_corr_data, coord.xyzp(:,1:2));
     
     tab_data_pat.Power = power_sim.*power_corr;
-       
+    
     powers_all{n_pat} = tab_data_pat.Power;
     tab_data(tab_data.Pattern == curr_pat,:) = tab_data_pat;
 end
