3ada51eadf43fc5966acad3da465ecc3a05cd546
yuriy <shymkivy@gmail.com>
2023-03-09 12:24:02 -0500

    bugs

--- SLM_GUI_funcions/BNS/f_SLM_update.m
+++ SLM_GUI_funcions/BNS/f_SLM_update.m
@@ -1,16 +1,25 @@
 function f_SLM_update(ops, image_pointer)
 
 if ops.SDK_created
+    
     if strcmpi(ops.SLM_type, 'BNS1920')
-        f_SLM_BNS1920_update(ops, image_pointer);
-    elseif strcmpi(ops.SLM_type, 'BNS1920_sdk3')
-        f_SLM_BNS1920_sdk3_update(ops, image_pointer);
+        if ~ops.sdk3_ver
+            f_SLM_BNS1920_sdk4_update(ops, image_pointer);
+        else
+            f_SLM_BNS1920_sdk3_update(ops, image_pointer);
+        end
     elseif strcmpi(ops.SLM_type, 'BNS512OD')
-        f_SLM_BNS512OD_update(ops, image_pointer);
-    elseif strcmpi(ops.SLM_type, 'BNS512OD_sdk3')
-        f_SLM_BNS512OD_sdk3_update(ops, image_pointer);
+        if ~ops.sdk3_ver
+            f_SLM_BNS512OD_sdk4_update(ops, image_pointer);
+        else
+            f_SLM_BNS512OD_sdk3_update(ops, image_pointer);
+        end
     elseif strcmpi(ops.SLM_type, 'BNS512')
-        f_SLM_BNS512_update(ops, image_pointer);
+        if ~ops.sdk3_ver
+            f_SLM_BNS512_sdk4_update(ops, image_pointer);
+        else
+            f_SLM_BNS512_sdk3_update(ops, image_pointer);
+        end
     else
         error('Undefined SLM in f_SLM_update');
     end
