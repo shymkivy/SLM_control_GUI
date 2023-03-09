d6d2ff0fde32ca8cdbfeca9dedba932ab97c33a4
yuriy <shymkivy@gmail.com>
2023-03-09 12:42:08 -0500

    bugs

--- SLM_GUI_funcions/BNS/f_SLM_BNS1920_sdk4_update.m
+++ SLM_GUI_funcions/BNS/f_SLM_BNS1920_sdk4_update.m
@@ -1,10 +1,10 @@
 function f_SLM_BNS1920_sdk4_update(ops, image_pointer)
 
 % loads image
-calllib('Blink_C_wrapper', 'Write_image', ops.board_number, image_pointer,...
+val_write = calllib('Blink_C_wrapper', 'Write_image', ops.board_number, image_pointer,...
         ops.width*ops.height, ops.wait_For_Trigger, ops.flip_immediate,...
         ops.external_Pulse, ops.output_pulse_image_refresh, ops.timeout_ms);
 % checks if image is complete
-calllib('Blink_C_wrapper', 'ImageWriteComplete', ops.board_number, ops.timeout_ms);
+val_complete = calllib('Blink_C_wrapper', 'ImageWriteComplete', ops.board_number, ops.timeout_ms);
 
 end
