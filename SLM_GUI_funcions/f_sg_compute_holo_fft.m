c26d97f3d196d621f27d09e2306c98983a758463
yuriy <shymkivy@gmail.com>
2023-03-14 21:52:15 -0400

    updates

--- SLM_GUI_funcions/f_sg_compute_holo_fft.m
+++ SLM_GUI_funcions/f_sg_compute_holo_fft.m
@@ -35,11 +35,46 @@ pupil_amp = A  * exp(-res);
 %holo_image1 = phase_sq;
 %holo_image1(pupil_mask) = holo_image;
 
-SLM_complex_wave=pupil_amp.*exp(1i.*(holo_image - defocus_dist.*defocus*1e-6));
+holo_image2 = holo_image - defocus_dist.*defocus*1e-6;
+sim_crosstalk = 1;
+bin_fac = 5;
+
+if sim_crosstalk
+    holo_image_rep = f_repmat_xy(holo_image2, bin_fac);
+    holo_image3 = f_smooth_nd(holo_image_rep, [2 2]);
+
+    pupil_amp2 = f_repmat_xy(pupil_amp, bin_fac)/bin_fac^2;
+
+    % temp_cpx = exp(1i*holo_image2);
+    % mean_off_mag_pre = abs(mean(mean(temp_cpx)));
+    % for n = 1:10
+    %     mean_off = mean(mean(temp_cpx));
+    %     temp_cpx = temp_cpx - mean_off;
+    % end
+    % mean_off_mag_post = abs(mean(mean(temp_cpx)));
+    % 
+    % holo_image3 = angle(temp_cpx);
+    % temp_cpx = exp(1i*holo_image3);
+    % mean_off_mag_post2 = abs(mean(mean(temp_cpx)));
+else
+    holo_image3 = holo_image2;
+    pupil_amp2 = pupil_amp;
+end
+SLM_complex_wave=pupil_amp2.*exp(1i.*(holo_image3));
 
 im1 = fftshift(fft2(SLM_complex_wave));
 im_amp = abs(im1)/prod(dims);
 
+if sim_crosstalk
+    %im_amp = f_bin_sum(im_amp, bin_fac);
+    
+    dims2 = size(im_amp);
+    
+    idx1 = (dims2(1)/2 - dims(1)/2+1):(dims2(1)/2 + dims(1)/2);
+    idx2 = (dims2(2)/2 - dims(2)/2+1):(dims2(2)/2 + dims(2)/2);
+    im_amp = im_amp(idx1 ,idx2);
+    
+end
 % figure()
 % imagesc(im_amp)
 
@@ -47,7 +82,27 @@ im_amp = abs(im1)/prod(dims);
 %y_lab = linspace(-(ph_d-1)/2, (ph_d-1)/2, dims(1)-1)/2;
 
 % fft output, first value is baseline (0, 0), the middle of remainder is N/2 freq
+
+dims = size(im_amp);
+
 x_lab = round(((1:dims(2))-(dims(2)/2)-1)/2/dims(2)*ph_d,2);
 y_lab = round(((1:dims(1))-(dims(1)/2)-1)/2/dims(1)*ph_d,2);
+
+end
+
+function mat_out = f_repmat_xy(mat_in, num_rep)
+
+[d1, d2] = size(mat_in);
+holo_image2 = reshape(permute(repmat(mat_in, [1, 1, num_rep]), [3, 1, 2]), [d1*num_rep, d2]);
+[d1, d2] = size(holo_image2);
+mat_out = reshape(permute(repmat(holo_image2, [1, 1, num_rep]), [3, 2, 1]), [d2*num_rep, d1])';
+
+end
+
+function mat_out = f_bin_sum(mat_in, num_bin)
+
+[d1, d2] = size(mat_in);
+mat_in2 = reshape(sum(reshape(mat_in, [num_bin, d1/num_bin, d2]),1), [d1/num_bin, d2]);
+mat_out = reshape(sum(reshape(mat_in2', [num_bin, d2/num_bin, d1/num_bin]),1), [d2/num_bin, d1/num_bin])';
 
 end
