959cdadb081d92dcff5b0fe787116798d7a13010
yuriy <shymkivy@gmail.com>
2024-04-29 11:05:52 -0400

    updates

--- SLM_GUI_funcions/scan/f_sg_EOF_Zscan_trig.m
+++ SLM_GUI_funcions/scan/f_sg_EOF_Zscan_trig.m
@@ -1,9 +1,15 @@
 function scan_data  = f_sg_EOF_Zscan_trig(app, holo_pointers, num_planes_all, imaging_button, scans_per_frame)
 % end of frame scan, triggers sent to SLM
+% SLM needs end of frame trigger going down, to start pattern change
+% counter channel needs e
 
 if ~exist('scans_per_vol', 'var') || isempty(scans_per_frame)
     scans_per_frame = 1;
 end
+
+session = app.DAQ_session;
+resetCounters(session);
+pause(0.05);
 
 if scans_per_frame > 1
     holo_pointers2 = cell(scans_per_frame, size(holo_pointers,1));
@@ -12,6 +18,8 @@ if scans_per_frame > 1
     end
     holo_pointers3 = holo_pointers2(:);
     num_planes_all = num_planes_all * scans_per_frame;
+else
+    holo_pointers3 = holo_pointers;
 end
 
 imaging = true;
@@ -29,23 +37,31 @@ f_SLM_update(scan_ops, holo_pointers3{1});
 scan_ops.wait_For_Trigger = 1;
 f_SLM_update(scan_ops, holo_pointers3{2});
 SLM_frame = 2; % current SLM on bench
-scan_frame = 1; % current scan frame
+%scan_frame = 1; % current scan frame
 
+scan1 = inputSingleScan(session);
 disp('Ready to start imaging');
 last_time = toc;
 while imaging
-    if SLM_frame == scan_frame
-        f_SLM_update(scan_ops, holo_pointers3{rem(SLM_frame-1,num_planes)+1});
-        SLM_frame = SLM_frame + 1;
-    end
     
-    % waits for write
-    write_complete = f_SLM_BNS1920_write_complete(scan_ops);
-    if write_complete
-        frame_end_times(scan_frame) = toc;
-        scan_frame = scan_frame + 1;
-        last_time = frame_end_times(scan_frame);
+    scan_frame = scan1(1);
+
+    if SLM_frame == scan_frame
+        % load the next frame, which is SLM_frame+1
+        SLM_frame = SLM_frame + 1;
+        f_SLM_update(scan_ops, holo_pointers3{rem(SLM_frame-1,num_planes)+1});
+        frame_end_times(scan_frame-1) = toc;
+        last_time = frame_end_times(scan_frame-1);
     end
+    scan1 = inputSingleScan(session);
+
+    %pause(0.001);
+    % waits for write zero means it was success
+    %write_complete = f_SLM_BNS1920_write_complete(scan_ops);
+    %if write_complete
+    
+    %scan_frame = scan_frame + 1;
+    %end
       
     if scan_frame > num_planes_all
         imaging = 0;
@@ -61,7 +77,7 @@ while imaging
 end
 
 pause(1);
-scan_data.frame_end_times = frame_end_times;
+scan_data.frame_start_times = frame_end_times;
 scan_data.holo_pointers = holo_pointers;
 scan_data.scans_per_frame = scans_per_frame;
 scan_data.num_planes_all = num_planes_all;
